a3dcd3a179c052759d0c404ee408d56b5f17952d
kevinsims1 <kevinsims1@outlook.com>
2020-01-07 15:56:09 -0600

    components rendering

---
+++ src/components/directions.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Directions = () =>{
+    return(
+        <div>
+            Hello World
+        </div>
+    )
+}
+
+export default Directions
---
+++ src/components/inventory.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Inventory = () =>{
+    return(
+        <div>
+            Hello World
+        </div>
+    )
+}
+
+export default Inventory
