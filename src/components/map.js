4ee0bb6a3ead66fa4cd4c582f2302cf30b4a0409
kevinsims1 <kevinsims1@outlook.com>
2020-01-08 20:18:36 -0600

    rooms title and description rendering to client

--- src/components/map.js
+++ src/components/map.js
@@ -3,10 +3,23 @@ import axios from 'axios'
 
 import '../css/Map.css'
 
-const Map = (rooms) =>{
+const Map = () =>{
+    const [rooms, setRooms] = useState([])
+
+    useEffect(() => {
+        axios.get('https://mud-build.herokuapp.com/api/adv/rooms')
+        .then(res=>{
+            console.log('res',res.data)
+            setRooms(res.data)
+            console.log(rooms)
+        })
+        .catch(err => {
+            console.log("error", err)
+        })
+    }, [rooms])
     return(
         <div className="mapWrapper">
-            {rooms.name} 
+            {rooms.map(room => <div>{room.title}</div>)} 
         </div>
     )
 }
